ffbf3a943c04fb90fc45ec088ed9d2858fd667e2
Gavin Mendel-Gleason <gavin@terminusdb.com>
2022-08-25 19:03:07 +0200

    Restartable

--- load-files.py
+++ load-files.py
@@ -16,13 +16,20 @@ def optimize():
         subprocess.run(["terminusdb","optimize","admin/bdrc/_meta"])
         subprocess.run(["terminusdb","optimize","admin/bdrc/local/_commits"])
 
+RESTART_AFTER = ['/home/gavin/dev/bdrc/instances','43'] # set to [] to process everything
+RESTART_SEEN = False # set to True to process everything
 Dirs = [INSTANCE_DIR,WORKS_DIR]
 for Dir in Dirs:
     print(f"Loading {Dir}")
     for hash_dir in os.listdir(Dir):
         print(f"  hash_dir: {hash_dir}")
-        for filename in os.listdir(f"{Dir}/{hash_dir}"):
-            if filename.endswith(".trig"):
-                optimize()
-                print(f"    processing {filename}")
-                p = subprocess.run(["terminusdb", "triples", "load", "admin/bdrc/local/branch/main/instance", f"{Dir}/{hash_dir}/{filename}"])
+        print(f"{[Dirs,hash_dir]}")
+        if len(RESTART_AFTER) > 0 and [Dir,hash_dir] == RESTART_AFTER:
+            RESTART_SEEN = True
+            continue
+        elif RESTART_SEEN == True:
+            for filename in os.listdir(f"{Dir}/{hash_dir}"):
+                if filename.endswith(".trig"):
+                    optimize()
+                    print(f"    processing {filename}")
+                    p = subprocess.run(["terminusdb", "triples", "load", "admin/bdrc/local/branch/main/instance", f"{Dir}/{hash_dir}/{filename}"])
