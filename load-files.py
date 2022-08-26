1116edbd640ff4fdef670da1b4402662068841ba
Gavin Mendel-Gleason <gavin@terminusdb.com>
2022-08-26 09:21:09 +0200

    Better restart logic, better schema

--- load-files.py
+++ load-files.py
@@ -16,15 +16,18 @@ def optimize():
         subprocess.run(["terminusdb","optimize","admin/bdrc/_meta"])
         subprocess.run(["terminusdb","optimize","admin/bdrc/local/_commits"])
 
-RESTART_AFTER = ['/home/gavin/dev/bdrc/instances','a6'] # set to [] to process everything
+RESTART_AFTER = ['/home/gavin/dev/bdrc/instances','90'] # set to [] to process everything
 RESTART_SEEN = False # set to True to process everything
 Dirs = [INSTANCE_DIR,WORKS_DIR]
 for Dir in Dirs:
     print(f"Loading {Dir}")
-    for hash_dir in os.listdir(Dir):
+    hash_dirs = os.listdir(Dir)
+    for hash_dir in hash_dirs:
         print(f"  hash_dir: {hash_dir}")
-        print(f"{[Dirs,hash_dir]}")
-        if len(RESTART_AFTER) > 0 and [Dir,hash_dir] == RESTART_AFTER:
+        if (not RESTART_SEEN
+            and len(RESTART_AFTER) == 2
+            and Dirs.index(Dir) >= Dirs.index(RESTART_AFTER[0])
+            and hash_dirs.index(hash_dir) >= hash_dirs.index(RESTART_AFTER[1])):
             RESTART_SEEN = True
             continue
         elif RESTART_SEEN == True:
@@ -35,3 +38,6 @@ for Dir in Dirs:
                     CMD = ["terminusdb", "triples", "load", "admin/bdrc/local/branch/main/instance", f"{Dir}/{hash_dir}/{filename}"]
                     print(f"Running {CMD}")
                     p = subprocess.run(CMD)
+        else:
+            print(f"Skipping {Dir} and {hash_dir}")
+
